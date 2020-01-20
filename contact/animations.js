16be5f319000664a941ef7521a25ba91bed1a483
Oscar <36861657+rev-57@users.noreply.github.com>
2020-01-20 11:53:22 +0000

    added fancy animations and updated info

--- contact/animations.js
+++ contact/animations.js
@@ -1,46 +1,36 @@
 function animate_logo() {
   if (window.toggle == 0) {
-    window.title.innerText = "> contact me_";
+    window.title.innerText = "> contact_me";
     window.toggle = 1;
   } else {
-    window.title.innerText = "> contact me";
+    window.title.innerText = "> contact_me_";
     window.toggle = 0;
   }
 }
-
-//function create_title() {
-//  target_string = "> rev-57";
-//  window.title.innerText += target_string[i];
-//  for (i=0;i<target_string.length;i++) {
-//    if (target_string[i] == ">") {
-//      window.title.innerText += "> r";
-//      i += 2;
-//    } else {
-//      window.title.innerText += target_string[i];
-//    }
-//  }
-//}
-
-//function initialise_logo() {
-//  var target_string = "> rev-57";
-//  for (i = 0; i < target_string.length; i++) {
-//    setTimeout(function() {window.title.innerText[i] = target_string[i];}, 500);
-//    console.log(window.title.innerText);
-//  }
-
-  //setInterval("animate_logo()", 500);
-//}
 
 function on_page_load() {
   window.toggle = 0;
   window.title = document.getElementById("title");
 
-  //var elstring = "";
-  //for (i=0;i<10;i++) {
-  //  elstring += i;
-  //  console.log(elstring);
-  //}
-  //create_title();
-  //initialise_logo();
   setInterval("animate_logo()", 500);
+}
+
+function logo_startup() {
+  var title = document.getElementById("title");
+  var pos = 0;
+  var target_string = "contact_me";
+  var id = setInterval(frame, 100);
+  function frame() {
+    if (pos == 0) {
+      title.innerText += " c_"
+      pos++;
+    } else if (pos < target_string.length && pos > 0) {
+      title.innerText = title.innerText.slice(0, title.innerText.length-1);
+      title.innerText += target_string[pos] + "_";
+      pos++;
+    } else {
+      clearInterval(id);
+      on_page_load();
+    }
+  }
 }
